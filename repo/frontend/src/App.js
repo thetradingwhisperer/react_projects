1a1b496e0d08c8dfef6b668290e48e2cc014a585
The Trading Whisperer <29692338+thetradingwhisperer@users.noreply.github.com>
2022-02-10 16:34:08 +0000

    new files

--- repo/frontend/src/App.js
+++ repo/frontend/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <Headers />
-      <main>
+      <main className='py-3'>
         <Container>
          <h1>Welcome to IvoireShop</h1>
         </Container>
